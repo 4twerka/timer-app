caf7bca661f7fdf38f998402c862163f1b9ac533
Artem Posudevskyi <penquin4m@gmail.com>
2024-10-08 23:44:32 +0300

    Second commit

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Main() {
+  const [seconds, setSeconds] = useState(0);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const startStopWatch = () => {
+    setIsClicked(!isClicked);
+  }
+  useEffect(() => {
+    let stopWatch = null;
+
+    if (isClicked) {
+      stopWatch = setInterval(() => {
+        setSeconds(prev => prev + 1);
+      }, 1000);
+   } else if (!isClicked && seconds !== 0) {
+      clearInterval(stopWatch);
+    }
+
+    return () => clearInterval(stopWatch);
+  }, [isClicked]);
 
 
     return (
@@ -47,13 +66,13 @@ function Main() {
             <div>
               <h2 className="text-2xl font-semibold text-gray-600 text-center mb-4">Stopwatch</h2>
               <div className="text-center text-4xl font-bold text-gray-700 mb-6">
-                00:00:00
+                {seconds}
               </div>
               <div className="flex justify-center space-x-4">
-                <button className="bg-blue-500 text-white font-semibold py-2 px-6 rounded shadow hover:bg-blue-600 transition">
+                <button onClick={startStopWatch} className="bg-blue-500 text-white font-semibold py-2 px-6 rounded shadow hover:bg-blue-600 transition">
                   Start
                 </button>
-                <button className="bg-yellow-500 text-white font-semibold py-2 px-6 rounded shadow hover:bg-yellow-600 transition">
+                <button onClick={} className="bg-yellow-500 text-white font-semibold py-2 px-6 rounded shadow hover:bg-yellow-600 transition">
                   Stop
                 </button>
                 <button className="bg-red-500 text-white font-semibold py-2 px-6 rounded shadow hover:bg-red-600 transition">
